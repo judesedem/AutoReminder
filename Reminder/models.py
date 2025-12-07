a80618b794654cac8515e82bb7f4c4a39b8dbfd5
Jude Fiadzawoo <judefiadzawoo@gmail.com>
2025-12-07 20:55:28 +0000

    Added the models I could think of

--- Reminder/models.py
+++ Reminder/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from account import User
-from django.utils import timezone
+
 
 class Reminder(models.Model):
     reminder=models.ForeignKey(User, on_delete=models.CASCADE)
     note=models.TextField(null=True, blank=True)
     title=models.CharField(max_length=100)
-    date_added=models.DateTimeField(default=timezone)
+    created_at=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return f"{ self.title}-{self.reminder}"
