4f453a1acc9cb0103f11db5378bfe3b8dde5fd4d
Jude Fiadzawoo <judefiadzawoo@gmail.com>
2025-12-11 08:48:31 +0000

    completed views and fixed errors in urls

--- AutoReminder/settings.py
+++ AutoReminder/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework.simplejwt",
+    "rest_framework_simplejwt",
     "Reminder"
 ]
 
--- Reminder/views.py
+++ Reminder/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .serializers import UserSerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User=get_user_model()
+
+class RegisterView(CreateAPIView):
+    serializer_class=UserSerializer
+    permission_classes=[AllowAny]
+    queryset=User.objects.all()
