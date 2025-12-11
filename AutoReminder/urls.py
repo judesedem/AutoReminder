07248ea5a77736828c58933798bfda3391f8a900
Jude Fiadzawoo <judefiadzawoo@gmail.com>
2025-12-11 03:06:12 +0000

    Completed serializers for user and reminder

--- AutoReminder/urls.py
+++ AutoReminder/urls.py
@@ -1,8 +1,7 @@
 from django.urls import re_path
-from . import views
+ 
 
 urlpatterns=[
-    re_path('signup',views.signup),
-    re_path('login',views.login)
+  
     
 ]
--- Reminder/serializers.py
+++ Reminder/serializers.py
@@ -1,13 +1,31 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 from .models import Reminder
 
+User=get_user_model()
 class UserSerializer(serializers.ModelSerializer):
+    password=serializers.CharField(write_only=True)
     class Meta(object):
         model=User
         fields=['username','email','password']
+    
+    def create(self, validated_data):
+        user=User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
 
-class ReminderSerializer(serializers.ModelSerializer):
-    class Meta(object):
+class ReminderSerializer(serializers.ModelSerializer):    
+    class Meta:
         model=Reminder
-        fields=['title','note','reminditem.username']
+        fields='__all__'
+        read_only_fields=['user','id']
+
+    def validate_scheduled_time(self,value):
+        if value<timezone.now():
+            raise serializers.ValidationError("Set an appropriate date")
+
+        return value
